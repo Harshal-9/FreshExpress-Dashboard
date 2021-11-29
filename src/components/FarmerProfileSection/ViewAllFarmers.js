5361feb7e02c495062a5c2c6c921414928620626
Harshal-9 <harshal.kodgire@walchandsangli.ac.in>
2021-11-29 23:13:08 +0530

    Added multiselect filter and fetched the data/options for reactselect from backend

--- src/components/FarmerProfileSection/ViewAllFarmers.js
+++ src/components/FarmerProfileSection/ViewAllFarmers.js
@@ -7,6 +7,7 @@ import Select, { components } from "react-select";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { MultiSelect } from "react-multi-select-component";
 
 library.add(faSearch);
 
@@ -24,7 +25,6 @@ const DropdownIndicator = (props) => {
 
 function SingleFarmerRow(props) {
   let farmerData = props;
-  console.log(props);
 
   const navigate = useNavigate();
   return (
@@ -59,6 +59,23 @@ function ViewAllFarmers() {
   const navigate = useNavigate();
   const [allFarmersArray, setAllFarmersArray] = useState([]);
 
+  const [selectedMultiSelectVillage, setSelectedMultiSelectVillage] = useState(
+    []
+  );
+  const [selectedMultiSelectTag, setSelectedMultiSelectTag] = useState([]);
+  const [selectedMultiSelectVariety, setSelectedMultiSelectVariety] = useState(
+    []
+  );
+
+  const [allFiltersData, setAllFiltersData] = useState({
+    GGN: [],
+    farmerName: [],
+    MHCode: [],
+    village: [],
+    variety: [],
+    tag: [],
+  });
+
   useEffect(() => {
     axios
       .get("https://immense-beach-88770.herokuapp.com/farmers")
@@ -82,6 +99,37 @@ function ViewAllFarmers() {
       .catch((err) => {
         console.log(err);
       });
+    axios
+      .get("https://immense-beach-88770.herokuapp.com/filters")
+      .then((data) => {
+        console.log("Data", data.data[0]);
+        setAllFiltersData(data.data[0]);
+
+        const recievedObj = data.data[0];
+        delete recievedObj._id;
+        const makeDataForOptions = {
+          GGN: [],
+          farmerName: [],
+          MHCode: [],
+          village: [],
+          variety: [],
+          tag: [],
+        };
+        for (let item in recievedObj) {
+          for (let arrItem in recievedObj[item]) {
+            console.log(recievedObj[item][arrItem], item);
+            makeDataForOptions[item].push({
+              label: recievedObj[item][arrItem],
+              value: recievedObj[item][arrItem],
+            });
+          }
+        }
+        console.log("makeDataForOptions", makeDataForOptions);
+        setAllFiltersData(makeDataForOptions);
+      })
+      .catch((err) => {
+        console.log("Err", err);
+      });
   }, []);
 
   return (
@@ -89,25 +137,60 @@ function ViewAllFarmers() {
       <br />
       <h2>ALL FARMERS</h2>
       <br />
+      <br />
       <Select
         className="searching"
         placeholder="Search Farmer Name"
         components={{ DropdownIndicator }}
+        options={allFiltersData.farmerName}
       />
       <Select
         className="searching"
         placeholder="Search GGN"
         components={{ DropdownIndicator }}
+        options={allFiltersData.GGN}
       />
       <Select
         className="searching"
         placeholder="Search MHCode"
         components={{ DropdownIndicator }}
+        options={allFiltersData.MHCode}
       />
       <br />
       <br />
-      <Select className="filter" placeholder="Search Village" />
-      <Select className="filter" placeholder="Search Tags" />
+      <MultiSelect
+        className="filter"
+        options={allFiltersData.village}
+        value={selectedMultiSelectVillage}
+        onChange={setSelectedMultiSelectVillage}
+        // isLoading={true}
+        // isCreatable={true}
+        // shouldToggleOnHover={true}
+        overrideStrings={{
+          selectSomeItems: "Filter Village",
+          allItemsAreSelected: "All villages selected",
+        }}
+      />
+      <MultiSelect
+        className="filter"
+        options={allFiltersData.tag}
+        value={selectedMultiSelectTag}
+        onChange={setSelectedMultiSelectTag}
+        overrideStrings={{
+          selectSomeItems: "Filter Tags",
+          allItemsAreSelected: "All tags selected",
+        }}
+      />
+      <MultiSelect
+        className="filter"
+        options={allFiltersData.variety}
+        value={selectedMultiSelectVariety}
+        onChange={setSelectedMultiSelectVariety}
+        overrideStrings={{
+          selectSomeItems: "Filter variety",
+          allItemsAreSelected: "All varieties selected",
+        }}
+      />
       <br />
       <br />
       <div class="AllFarmersScroll">
